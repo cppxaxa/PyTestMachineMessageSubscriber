dcf4ae5e239464f5e1431caa21be874ec530e535
Christ Prateek Prasanna Xaxa <cppxaxa@live.com>
2019-10-28 12:36:39 +0530

    first commit

---
+++ PyTestClient.py
@@ -0,0 +1,26 @@
+import requests
+from flask import Flask, jsonify, request, Request
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def accept():
+    # Request.
+    body = request.get_json()
+    print(body)
+    return jsonify("Hello")
+
+hubHost = "localhost"
+
+def PostResult(Host, Payload, port = 20000, Uri = None):
+    try:
+        if Uri == None:
+            Uri = "http://" + Host + ":" + str(port) + "/MachineMessageApi"
+        r = requests.post(Uri, json=Payload)
+        return True
+    except:
+        return False
+
+if __name__ == "__main__":
+    res = PostResult(hubHost, "AddMachineMessageSubscriber,127.0.0.1,5008")
+    print("Subscribed:", res)
+    app.run(port=5008)
